1f8ccd48ed61978e2cc7def02516329af3c47295
Chin-Hwa <randomchinhwa@gmail.com>
2024-04-13 16:13:24 -0500

    asdf

--- stack_dim.py
+++ stack_dim.py
@@ -3,6 +3,12 @@ import pandas as pd
 dataset = {'Entity': [100000, 200000, '300000'],
            'ICP': [199999, 299999, '399999']}
 
-print(type(dataset))
-print(dataset)
+account = ['0001', '0002', '0003', '2140']
+
+# pd.melt() method unpivots the dataframe
+#my_df = pd.melt(my_df, ignore_index = False).reset_index()
+
+sample_set = pd.DataFrame(dataset)
+
+print(sample_set)
 
