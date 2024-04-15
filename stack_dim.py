8d5f1a7a149dc40ebcbdb039bea14167abc882f7
Chin-Hwa <randomchinhwa@gmail.com>
2024-04-15 07:38:00 -0500

    Add melt method

--- stack_dim.py
+++ stack_dim.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import os
 
 dataset = {'Entity': [100000, 200000, '300000'],
-           'ICP': [199999, 299999, '399999']}
-
-account = ['0001', '0002', '0003', '2140']
-
-# pd.melt() method unpivots the dataframe
-#my_df = pd.melt(my_df, ignore_index = False).reset_index()
+           'ICP': [199999, 299999, '399999'],
+           '0001': [234.40, 0, 2134.00],
+           '0002': [2144.22, 422.00, 0],
+           '2140': [993.00, 200, 24.00]}
 
 sample_set = pd.DataFrame(dataset)
 
-print(sample_set)
+# pd.melt() method unpivots the dataframe
+new_set = pd.melt(sample_set, id_vars = ['Entity', 'ICP'],
+        var_name = 'Account', value_name= 'Amount')
 
+print(new_set)
