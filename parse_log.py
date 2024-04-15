25a8a7f5bfeeb6b5dfa0143e9157e5659407aca9
Chin-Hwa <randomchinhwa@gmail.com>
2024-04-15 16:27:02 -0500

    Add bad parsing

--- parse_log.py
+++ parse_log.py
@@ -6,7 +6,21 @@ selected_file = "dummy.log"
 # Open() method with the "r", "a", "w', and "x" arguemnts will
     # make it read-only, append, write, and create respectively
 
-working_file = open(selected_file, "r")
+# Be sure to uncomment the .close() below if using this one
+#working_file = open(selected_file, "r")
+
+
+#  *** KEEP the Code Snippet for reference!!!***
+
+with open(selected_file) as f:
+    my_data = []
+    lines = f.readlines()
+    for line in lines:
+        my_data.append(re.split(";",line.strip()))
+
+sample_df = pd.DataFrame(my_data)
+
+sample_df.to_excel("output.xlsx")
 
 '''
 with open(selected_file) as f:
@@ -19,6 +33,8 @@ for line in f:
             break
 '''
 
-print(working_file.read())
+# This just helps check to see if the log file I'm reading
+    # is still reading.
+#print(working_file.read())
 
-working_file.close()
+#working_file.close()
--- stack_dim.py
+++ stack_dim.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 dataset = {'Entity': [100000, 200000, '300000'],
            'ICP': [199999, 299999, '399999'],
