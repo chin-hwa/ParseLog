6def0cbd3b6f4ba3365e5064bf939c65a5eecb06
Chin-Hwa <randomchinhwa@gmail.com>
2024-07-22 20:53:04 -0500

    Create 'Error Value' column

--- parse_log.py
+++ parse_log.py
@@ -1,6 +1,7 @@
 import re
 # FYI, "re" library is reg ex
 import pandas as pd
+import numpy as np
 
 selected_file = "dummy.log"
 
@@ -23,38 +24,35 @@ with open(selected_file) as f:
     for line in lines:
         my_data.append(re.split(";",line.strip()))
 
-# There are 13 columns to output originally
-# Open to add the other columns
+# There are 13 columns to output originally.  Need 3 more columns
 dimensions = ['Scenario', 'Year', 'Period', 'View', 'Entity',
               'Value', 'Account', 'ICP', 'Custom1', 'Custom2',
-              'Custom3', 'Custom4','Amount']#,'Error Value','Error Message']
+              'Custom3', 'Custom4','Amount']
 sample_df = pd.DataFrame(my_data, columns = dimensions)
 
 # Only keep lines with 'ERROR' in the DataFrame
 error_only_df = sample_df[sample_df['Scenario'].str.contains('ERROR')]
-error_only_df.reset_index(drop = True, inplace = True)
+#error_only_df.reset_index(drop = True, inplace = True)
+
+# Remove the 1st and last line of "ERROR" lines
 middle_df = error_only_df.drop(error_only_df.head(1).index)\
                          .drop(error_only_df.tail(1).index)
 
+# Cut off the leftmost 37 characters of the 'Scenario' column
 middle_df['Scenario'] = middle_df['Scenario'].str[37:]
 
+# Create a new column called 'Error Values' from
+    # the 3rd row in the 'Scenario' column
+raw_EV = []
+for values in middle_df['Scenario']:
+    if ">>>>>>" in values:
+        raw_EV.append(values)
+    else:
+        raw_EV.append('')
 
+raw_EV = [elem.replace('>>>>>>', '') for elem in raw_EV]
+print(raw_EV)
 
-#middle_df.to_excel("output14.xlsx")
-
-'''
-with open(selected_file) as f:
-    f = f.readlines()
-
-for line in f:
-    for phrase in keep_phrases:
-        if phrase in line:
-            important.append(line)
-            break
-'''
-
-# This just helps check to see if the log file I'm reading
-    # is still reading.
-#print(working_file.read())
+#middle_df.to_excel("output3.xlsx")
 
 #working_file.close()
